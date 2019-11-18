33aff65a3e5d1a409de0163c3ee7714b54216b80
Oladehinde Kazeem <larrysnet2001@gmail.com>
2019-11-18 13:18:51 +0100

    chore(react-router-dom): setup application routing
    - install react-router-dom
    - create dummy pages/views
    - create routing
    
    [Maintains #card-29355418]

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -10,7 +11,9 @@ import { store, persistor } from './store';
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </PersistGate>
   </Provider>
   , document.getElementById('root'));

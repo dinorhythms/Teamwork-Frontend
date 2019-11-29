94b4c2cb5e78347b1919a14b32dc91d4f8497a3f
Oladehinde Kazeem <larrysnet2001@gmail.com>
2019-11-29 21:19:38 +0100

    Updates

---
+++ precache-manifest.e0c4b083a8f7198f31137195bd2a0ad8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcc0c126e8242ab494a99c6c618f070e",
+    "url": "/teamwork-frontend/index.html"
+  },
+  {
+    "revision": "4af37a0c63c65b756164",
+    "url": "/teamwork-frontend/static/css/main.d7b1aeb7.chunk.css"
+  },
+  {
+    "revision": "dcee82864d71dee6ce14",
+    "url": "/teamwork-frontend/static/js/2.7987be02.chunk.js"
+  },
+  {
+    "revision": "4af37a0c63c65b756164",
+    "url": "/teamwork-frontend/static/js/main.ea0ca0d1.chunk.js"
+  },
+  {
+    "revision": "9030b45cb4529a6be40f",
+    "url": "/teamwork-frontend/static/js/runtime-main.621c0826.js"
+  },
+  {
+    "revision": "4fe6a8132fee102480236ec8bfd59ecf",
+    "url": "/teamwork-frontend/static/media/confetti-doodles.4fe6a813.svg"
+  },
+  {
+    "revision": "99d8f09c4af0bc6515cc3e5776003f13",
+    "url": "/teamwork-frontend/static/media/login.99d8f09c.svg"
+  }
+]);
